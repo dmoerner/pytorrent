44054a355846795f297657c195312298f9029e41
Daniel Moerner <dmoerner@gmail.com>
2025-02-07 10:00:43 -0500

    Refactor into a main() function.
    Remove global variables and pass them as arguments. Reorganize
    unencapsulated code into a main() function. Add some basic docstrings
    and comments to functions.

--- main.py
+++ main.py
@@ -53,6 +53,19 @@ class TimeoutError(Exception):
 
 
 def decode_torrentfile(filename: str) -> dict:
+    """
+    Given a torrent filename, return a bdecoded dict. Sample output:
+
+    {b'announce': b'http://localhost:8080/announce',
+    b'created by': b'mktorrent 1.1',
+    b'creation date': 1737047340,
+    b'info':
+        {b'length': 12, b'name': b'data.txt',
+        b'piece length': 262144,
+        b'pieces': b'"Ycc\xb3\xde@\xb0o\x98\x1f\xb8]\x821.\x8c\x0e\xd5\x11',
+        b'private': 1}
+    }
+    """
     with open(filename, "rb") as f:
         decoded = bencoder.decode(f.read())
         if not isinstance(decoded, dict):
@@ -60,7 +73,12 @@ def decode_torrentfile(filename: str) -> dict:
         return decoded
 
 
-def construct_get_request(info: dict, peer_id: bytes, port=6881) -> dict:
+def construct_announce(info: dict, port=6881) -> dict:
+    """
+    Given a torrent info dict and your port, construct the
+    params for a tracker announce.
+    """
+    peer_id = b"ptc-0.1-" + random.randbytes(12)
     info_bencoded = bencoder.encode(info)
     info_hash = sha1(info_bencoded).digest()
     return {
@@ -76,10 +94,8 @@ def construct_get_request(info: dict, peer_id: bytes, port=6881) -> dict:
 
 def handle_recv(s: socket.socket) -> bytes:
     """
-    len = s.recv(4)
-    do something to convert len into an int
-    data = s.recv(len)
-    return data
+    Read a length-prefixed message, ignoring keepalives, and return the message
+    in bytes.
     """
     len_data = s.recv(4)
     length = int.from_bytes(len_data, byteorder="big")
@@ -96,10 +112,10 @@ def handle_recv(s: socket.socket) -> bytes:
     return data
 
 
-def handle_data(value_t: int, payload=b"") -> bytes:
-    """2
-    "\x00\x00\x01="
-    "\x00\x00\x01=\x05\xff\xff\xff"
+def construct_peer_msg(value_t: int, payload=b"") -> bytes:
+    """
+    Given a message type and a byte payload, return a length-prefixed
+    message.
     """
     return (
         int.to_bytes(len(payload) + 1, byteorder="big", length=4)
@@ -108,96 +124,10 @@ def handle_data(value_t: int, payload=b"") -> bytes:
     )
 
 
-"""
-torrent file: "length" means total size of data
-so pieces count = length / piece length
-
-BUT, in the messages that we send to a peer: "length" means the length
-of a block of a piece.
-
-{b'announce': b'http://localhost:8080/announce', 
-b'created by': b'mktorrent 1.1', 
-b'creation date': 1737047340, 
-b'info': 
-    {b'length': 12, b'name': b'data.txt', 
-    b'piece length': 262144, 
-    b'pieces': b'"Ycc\xb3\xde@\xb0o\x98\x1f\xb8]\x821.\x8c\x0e\xd5\x11', 
-    b'private': 1}
-}
-
-index: 0, begin: 0, length: 12
-
-other torrent file: piece length: 262_144, file_length: 300_000
-
-while requesting piece 1: index 0, begin 0, begin 16384, begin 32768, etc. block_length: 2**14 = 16_384
-while requesting piece 2: index 1, being length: 2**14, EXCEPT on the last block,
-length would be 300000 % 2**14 = 5088 to get the remainder that's left.
-
-begin = range(0, piece_length, block_length)
-
-300_000 - 262_144
-
-block_length = 2**14
-begin = 0
-piece = 0 # is actually what we are looping over in a higher for loop
-data_left = piece_length
-while data_left > 0:
-    if piece == piece_count - 1 and data_left < block_length:
-        block_length = data_left
-    make request (piece, begin, block_length)
-    begin += block_length
-    data_left -= block_length
-
-"""
-decoded_t = decode_torrentfile("./debian-12.9.0-amd64-netinst.iso.torrent")
-url = decoded_t[b"announce"].decode("utf-8")
-# Making a GET request to the tracker
-params = construct_get_request(decoded_t[b"info"], random.randbytes(20))
-requests.packages.urllib3.util.connection.HAS_IPV6 = False
-
-"""
-response: b'd8:interval4:270012:min interval2:305:peers6:\x7f\x00\x00\x01\x1a\xe1e'
-"""
-response = requests.get(url, params=params)
-decoded_response = bencoder.decode(response.content)
-peers = decoded_response[b"peers"]
-peers_list = [peers[i : i + 6] for i in range(0, len(peers), 6)]
-
-# TODO: change later
-# b'\x7f\x00\x00\x01\x1a\xe1
-# first_peer = peers_list[0]
-# peer_ip = ipaddress.IPv4Address(first_peer[:4]).exploded
-# peer_port = int.from_bytes(first_peer[4:], byteorder="big")
-# print("PEERS LIST: ",peers_list)
-# print("PEER IP:", peer_ip, "PEER PORT:", peer_port)
-# Connect to Peer
-"""
-1. Send handshake, receive handshake, verify handshake (verify protocol, verify info_hash)
-2. Send a message: Unchoked
-3. Download data
-"""
-# sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# PEER IP: 93.161.53.57 PEER PORT: 56251
-# sock.connect((peer_ip, peer_port))
-# sock.connect(("93.161.53.57", 56251))
-# payload_header = int.to_bytes(19, byteorder="big") + b"BitTorrent protocol"
-# payload = (
-#     payload_header + (int.to_bytes(0) * 8) + params["info_hash"] + params["peer_id"]
-# )
-
-
-# sock.sendall(payload)
-# handshake = sock.recv(68)
-# print("PEER RESPONSE:", handshake)
-
-# handshake_header = handshake[:20]
-# handshake_info_hash = handshake[28:48]
-
-# assert payload_header == handshake_header
-# assert params["info_hash"] == handshake_info_hash
-
-
 def wait_for_unchoke(s: socket.socket):
+    """
+    Peers start out in a choked state. Wait for a peer to unchoke.
+    """
     data = handle_recv(s)
     while data[0] != Message_Type.UNCHOKE:
         print("DATA:", data)
@@ -205,7 +135,7 @@ def wait_for_unchoke(s: socket.socket):
     print("DEBUG: wait_for_unchoke, data=", data)
 
 
-def handshake(peer_ip: socket._Address, peer_port: int) -> socket.socket:
+def handshake(params: dict, peer_ip: socket._Address, peer_port: int) -> socket.socket:
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((peer_ip, peer_port))
 
@@ -217,7 +147,6 @@ def handshake(peer_ip: socket._Address, peer_port: int) -> socket.socket:
     sock.sendall(payload)
     handshake = sock.recv(68)
     print("PEER RESPONSE:", handshake)
-
     handshake_header = handshake[:20]
     handshake_info_hash = handshake[28:48]
 
@@ -227,98 +156,19 @@ def handshake(peer_ip: socket._Address, peer_port: int) -> socket.socket:
     bitfield = handle_recv(sock)
     print("BITFIELD:", bitfield)
 
-    interested = handle_data(Message_Type.INTERESTED)
+    interested = construct_peer_msg(Message_Type.INTERESTED)
     sock.sendall(interested)
 
-    # data = handle_recv(sock)
-
-    # while data[0] != Message_Type.UNCHOKE:
-    #     print("DATA:", data)
-    #     data = handle_recv(sock)
     wait_for_unchoke(sock)
 
     return sock
 
 
-"""
-us: handshake
-them: handshake
-them: bitfield
-us: interested
-them: choked or unchoked
-us: (wait for unchoked)
-us: request a piece
-them: (transfer data)
-
-"""
-# TODO: change hard coded values
-# sock = handshake("93.161.53.57", 56251)
-
-# interested = handle_data(Message_Type.INTERESTED)
-# sock.sendall(interested)
-
-# data = handle_recv(sock)
-
-# while data[0] != Message_Type.UNCHOKE:
-#     print("DATA:", data)
-#     data = handle_recv(sock)
-
-""""
-  Piece Count: 2528
-  Piece Size: 256.0 KiB
-  Total Size: 662.7 MB
-
-  request: piece 0
-  we have nothing:
-  request: index = 0, begin = 0, length = 2^14
-  we get a block back
-  request: index = 0 begin = 2^14, length = 2^14
-  ...
-"""
-# begin = index = 0
-# length = 2**14
-# struct.pack('>III', index, begin, length)
-# int.to_bytes(begin, length=4, byteorder='big') + int.to_bytes(index, length=4, byteorder='big') + int.to_bytes(length, length=4, byteorder='big')
-# payload = struct.pack(">III", index, begin, length)
-# request_payload = handle_data(Message_Type.REQUEST, payload)
-# sock.sendall(request_payload)
-
-# data = handle_recv(sock)
-# print("REQUEST DATA:", data)
-
-"""
-aaaabbbb
-piece size = 4 (bytes)
-piece count = 2
-"""
-
-"""
-while requesting piece 0: index 0, begin 0, begin 16384, begin 32768, etc. block_length: 2**14 = 16_384
-while requesting piece 1: index 1, being length: 2**14, EXCEPT on the last block,
-length would be 300000 % 2**14 = 5088 to get the remainder that's left.
-
-begin = range(0, piece_length, block_length)
-
-300_000 - 262_144
-
-block_length = 2**14
-begin = 0
-index = 0 # is actually what we are looping over in a higher for loop
-data_left = piece_length
-while data_left > 0:
-    if index == piece_count - 1 and data_left < block_length:
-        block_length = data_left
-    make request (piece, begin, block_length)
-    begin += block_length
-    data_left -= block_length
-
-calculate piece hash
-verify that the piece hash matches the hash in the torrent file, otherwise retry (or error)
-the piece hash in the torrent file is a concatenation of hashes of each piece. so we 
-"""
-
-
 def verify_piece_hash(torrent_info: dict, piece_hash: bytes, index: int) -> bool:
+    """
+    Given the hash of a downloaded piece, verify that it matches the hash in the
+    torrent file.
+    """
     print("PIECE HASH:", piece_hash, len(piece_hash))
     torrent_piece_hash = torrent_info[b"info"][b"pieces"][index * 20 : index * 20 + 20]
     print("TORRENT PIECE HASH:", torrent_piece_hash, len(torrent_piece_hash))
@@ -327,6 +177,19 @@ def verify_piece_hash(torrent_info: dict, piece_hash: bytes, index: int) -> bool
 
 @timeout(30)
 def request_piece(s: socket.socket, torrent_info: dict, index: int) -> bytes:
+    """
+    Request a piece from a peer. The order of messages is:
+
+    us: handshake
+    them: handshake
+    them: bitfield
+    us: interested
+    them: choked or unchoked
+    us: (wait for unchoked)
+    us: request a piece
+    them: (transfer data)
+    """
+
     block_length = 2**14
     begin = 0
     piece_length = torrent_info[b"info"][b"piece length"]
@@ -340,7 +203,7 @@ def request_piece(s: socket.socket, torrent_info: dict, index: int) -> bytes:
         if index == piece_count - 1 and data_left < block_length:
             block_length = data_left
         payload = struct.pack(">III", index, begin, block_length)
-        request_payload = handle_data(Message_Type.REQUEST, payload)
+        request_payload = construct_peer_msg(Message_Type.REQUEST, payload)
         s.sendall(request_payload)
         requested_data = handle_recv(s)
 
@@ -366,8 +229,14 @@ def request_piece(s: socket.socket, torrent_info: dict, index: int) -> bytes:
     return data
 
 
-def request_file(torrent_info: dict, peer_ip: socket._Address, peer_port: int):
-    s = handshake(peer_ip, peer_port)
+def request_file(
+    params: dict, torrent_info: dict, peer_ip: socket._Address, peer_port: int
+):
+    """
+    Request a file.
+    """
+
+    s = handshake(params, peer_ip, peer_port)
     data = b""
     piece_length = torrent_info[b"info"][b"piece length"]
     file_size = torrent_info[b"info"][b"length"]
@@ -405,20 +274,34 @@ def request_file(torrent_info: dict, peer_ip: socket._Address, peer_port: int):
             wait_for_unchoke(s)
         except TimeoutError:
             # timeout error -> re-run handshake, update s to a new socket, then request_piece
-            s = handshake(peer_ip, peer_port)
+            s = handshake(params, peer_ip, peer_port)
 
     with open(file_name, "wb") as f:
         f.write(data)
 
 
-# data = request_piece(sock, decoded_t, 0)
-# print("REQUEST PIECE DATA:", data[:50])
-
-# request_file(decoded_t, "93.161.53.57", 56251)
-
-
 def main():
     print("Hello from torrentclient!")
+
+    decoded_t = decode_torrentfile("./debian-12.9.0-amd64-netinst.iso.torrent")
+
+    # Make announce
+    tracker_url = decoded_t[b"announce"].decode("utf-8")
+    params = construct_announce(decoded_t[b"info"])
+    response = requests.get(tracker_url, params=params)
+
+    # Decode peer list
+    decoded_response = bencoder.decode(response.content)
+    if not isinstance(decoded_response, dict):
+        raise ValueError("Did not receive dict from tracker")
+    peers = decoded_response[b"peers"]
+    peers_list = [peers[i : i + 6] for i in range(0, len(peers), 6)]
+
+    # Request File
+    # first_peer = peers_list[0]
+    # peer_ip = ipaddress.IPv4Address(first_peer[:4]).exploded
+    # peer_port = int.from_bytes(first_peer[4:], byteorder="big")
+    request_file(params, decoded_t, "93.161.53.57", 56251)
 
 
 if __name__ == "__main__":
