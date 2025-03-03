ba2bf466e190e2481ac914708081ee0b49c9cc52
Daniel Moerner <dmoerner@gmail.com>
2025-03-03 16:13:26 -0500

    Correct typo in percent complete calculation
    The JSON object has a size field, not a file_size field.

--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -89,7 +89,7 @@
 		<li>Name: {status.torrent_name}</li>
 		<li>
 			Percent complete: {Math.round(
-				(status.left / status.file_size) * 10000,
+				(status.left / status.size) * 10000,
 			) / 100}%
 		</li>
 		<li>Amount left: {status.left}</li>
